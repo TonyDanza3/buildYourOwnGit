513570ba63cd11d877767cabe9e7fada9987ed45
Михаил Миллер <MAMiller@alfabank.ru>
2025-04-30 13:40:45 +0200

    added getDir and getFileName methods

--- src/main/java/filesystem/FileUtils.java
+++ src/main/java/filesystem/FileUtils.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 import static java.util.stream.Collectors.toList;
 
@@ -19,7 +20,7 @@ public class FileUtils {
     }
 
     public static void checkIfFile(Path path) {
-        if (! new File(String.valueOf(path)).isFile()) {
+        if (!new File(String.valueOf(path)).isFile()) {
             throw new RuntimeException(path + " is not a file");
         }
     }
@@ -32,8 +33,23 @@ public class FileUtils {
         } catch (IOException e) {
             throw new RuntimeException("Could not get lines amount of file " + path + ": ", e);
         }
-        if(lineNumber > fileLinesAmount - 1) {
+        if (lineNumber > fileLinesAmount - 1) {
             throw new RuntimeException("Specified line number '" + lineNumber + "' exceeds file lines amount");
         }
+    }
+
+    public static String getDirectoryFromPath(Path dir) {
+        List<String> tokens = List.of(dir.toString().split("/"));
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < tokens.size() - 1; i++) {
+            result.append(tokens.get(i));
+            result.append("/");
+        }
+        return result.toString();
+    }
+
+    public static String getFileNameFromPath(Path dir) {
+        List<String> tokens = List.of(dir.toString().split("/"));
+        return tokens.get(tokens.size() - 1);
     }
 }
