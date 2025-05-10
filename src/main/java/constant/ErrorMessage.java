7d5ad853fdd8ce9c135aa80c1cf8155f36a177dd
tonyDanza3 <millermisha5@gmail.com>
2025-05-10 13:22:21 +0200

    added error message

--- src/main/java/constant/ErrorMessage.java
+++ src/main/java/constant/ErrorMessage.java
@@ -1,7 +1,8 @@
-package command.error;
+package constant;
 
 import java.util.Formatter;
 
 public class ErrorMessage {
     public static final String INVALID_COMMAND = "git: '%s' is not a git command. See 'git --help'.";
+    public static final String NOT_A_GIT_REPOSITORY = "fatal: not a git repository (or any of the parent directories): .git";
 }
