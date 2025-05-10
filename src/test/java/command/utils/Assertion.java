16cba34526b0ec4ed35c61e139670c80410eccbf
tonyDanza3 <millermisha5@gmail.com>
2025-05-10 13:21:35 +0200

    added template for command assertion in tests

---
+++ src/test/java/command/utils/Assertion.java
@@ -0,0 +1,16 @@
+package command.utils;
+
+import command.commands.Commands;
+
+import java.nio.file.Path;
+
+public class Assertion {
+    //TODO
+//    public static boolean checkCommandResult(Commands command, Path repoRootDir) {
+//
+//    }
+//
+//    private static boolean checkInitCommandResult() {
+//
+//    }
+}
