6770984c6aca5ade1c0681311e25b8b84c8b1f83
tonyDanza3 <millermisha5@gmail.com>
2025-05-11 17:13:13 +0200

    naive implementation of main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,7 @@
+import command.Command;
+import command.CommandFactory;
+import command.commands.Commands;
+import command.validator.CommandValidator;
 import filesystem.FileUtils;
 
 import java.nio.file.Path;
@@ -8,6 +12,10 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) {
-
+        CommandValidator commandValidator = new CommandValidator();
+        if (commandValidator.validate(args[0])) {
+            Command command = CommandFactory.createCommand(Commands.valueOf(args[0]));
+            command.execute();
+        }
     }
 }
