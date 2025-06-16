88f7edd8c69ae134e6e91cf8a59e7a762ff1eeaf
tonyDanza3 <millermisha5@gmail.com>
2025-06-16 11:24:20 +0200

    added todos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,5 +12,15 @@ public class Main {
             command.execute();
         }
     }
-// TODO: is it a good idea to add generateOutput() method to Command class?
+/* TODO:
+    STATUS command and possible cases
+        not a repo
+        does not have changed files (nothing to commit)
+        has changed unstaged files
+        has changed staged files
+
+    INIT command
+        (+)init new repo
+        reinitialize repo
+  */
 }
