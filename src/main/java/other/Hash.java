dacbea6a1886ba8448a0492c5504e413e99c0536
tonyDanza3 <millermisha5@gmail.com>
2025-06-23 21:04:22 +0200

    implemented hash class

---
+++ src/main/java/other/Hash.java
@@ -0,0 +1,25 @@
+package other;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Hash {
+    public static String getHashFromFile(File file) {
+        byte[] hashBytes;
+        StringBuilder hashString = new StringBuilder();
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-1");
+            hashBytes = md.digest(Files.readAllBytes(Path.of(file.getPath())));
+        } catch (NoSuchAlgorithmException | IOException e ) {
+            throw new RuntimeException("Could not get hash of file " + file + " because of the following exception: " + e);
+        }
+        for(byte byt: hashBytes) {
+            hashString.append(byt);
+        }
+        return hashString.toString();
+    }
+}
