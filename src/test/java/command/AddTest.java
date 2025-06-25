279d591438d44c38a3348fe2d1723dd1e07ec1ac
tonyDanza3 <millermisha5@gmail.com>
2025-06-25 14:05:19 +0200

    added draft checklist with tests

---
+++ src/test/java/command/AddTest.java
@@ -0,0 +1,15 @@
+package command;
+
+import org.junit.jupiter.api.Test;
+
+public class AddTest {
+
+    @Test
+    public void newFile() {}
+
+    @Test
+    public void alreadyAddedFile() {}
+
+    @Test
+    public void nonExistentFile() {}
+}
