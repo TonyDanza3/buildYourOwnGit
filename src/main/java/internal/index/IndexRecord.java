fda263a4ea060f629b801be7c65e4dcd1f4a2e48
tonyDanza3 <millermisha5@gmail.com>
2025-07-09 14:37:08 +0200

    added getter annotation and lombok plugin

--- src/main/java/internal/index/IndexRecord.java
+++ src/main/java/internal/index/IndexRecord.java
@@ -1,70 +1,19 @@
 package internal.index;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Builder
+@Getter
 public class IndexRecord {
-
-    public IndexRecord(IndexRecordBuilder builder) {
-        this.mode = builder.mode;
-        this.objectSHA = builder.objectSHA;
-        this.stagePhase = builder.stagePhase;
-        this.fileName = builder.fileName;
-    }
 
     private final Mode mode;
     private final String objectSHA;
     private final StagePhase stagePhase;
     private final String fileName;
 
-    public Mode getMode() {
-        return mode;
-    }
 
-    public String getObjectSHA() {
-        return objectSHA;
-    }
-
-    public StagePhase getStagePhase() {
-        return stagePhase;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public static IndexRecordBuilder builder() {
-        return new IndexRecordBuilder();
-    }
-
-    public static class IndexRecordBuilder {
-        private Mode mode;
-        private String objectSHA;
-        private StagePhase stagePhase;
-        private String fileName;
-
-        public IndexRecordBuilder mode(Mode mode) {
-            this.mode = mode;
-            return this;
-        }
-
-        public IndexRecordBuilder objectSHA(String objectSHA) {
-            this.objectSHA = objectSHA;
-            return this;
-        }
-
-        public IndexRecordBuilder stagePhase(StagePhase stagePhase) {
-            this.stagePhase = stagePhase;
-            return this;
-        }
-
-        public IndexRecordBuilder fileName(String fileName) {
-            this.fileName = fileName;
-            return this;
-        }
-
-        public IndexRecord build() {
-            return new IndexRecord(this);
-        }
-
-    }
 }
