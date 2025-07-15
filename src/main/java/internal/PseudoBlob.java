2640f1df7bd1310ac39635ac3b94f6dc302b377d
tonyDanza3 <millermisha5@gmail.com>
2025-07-15 22:29:15 +0200

    added PseudoBlob and one test

--- src/main/java/internal/PseudoBlob.java
+++ src/main/java/internal/PseudoBlob.java
@@ -1,17 +1,21 @@
 package internal;
 
 import filesystem.FileSystem;
+import filesystem.FileUtils;
 
+import java.io.File;
 import java.nio.file.Path;
 
 public class PseudoBlob {
-    public static void generatePseudoBlobFile(FileSystem fileSystem, Path filePath, String fileContents) {
-        fileSystem.createFile(filePath);
+
+    public static void generatePseudoBlobFile(FileSystem fileSystem, Path targetDir, File initialFile) {
+        String fileContents = fileSystem.getFileContentsAsString(initialFile.toPath());
+        fileSystem.createFile(targetDir);
         String blobContents = "blob " +
                 fileContents.length() +
                 " " +
                 "\\0" +
                 fileContents;
-        fileSystem.putContentToFile(filePath, blobContents);
+        fileSystem.putContentToFile(targetDir, blobContents);
     }
 }
--- src/test/java/internal/PseudoBlobTest.java
+++ src/test/java/internal/PseudoBlobTest.java
@@ -2,41 +2,67 @@ package internal;
 
 import command.commands.init.Init;
 import filesystem.FileSystem;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.util.List;
 
 import static constant.Names.GIT_FOLDER_NAME;
 import static constant.Names.OBJECTS_FOLDER_SUB_DIR;
+import static filesystem.utils.FileSystemTestUtils.recursivelyRemoveDirectory;
 import static filesystem.utils.TestData.RESOURCES_DIR;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class PseudoBlobTest {
 
-    private final Path rootDir = Path.of(RESOURCES_DIR + "/" + "pseudoBlobRepoDir");
+    private static final Path rootDir = Path.of(RESOURCES_DIR + "/" + "pseudoBlobRepoDir");
     private static final FileSystem fileSystem = new FileSystem();
     private final Init init = new Init(() -> rootDir, System.out::println);
 
     @BeforeAll
     public static void createRootDirectory() {
-        fileSystem.createDirectory(Path.of(RESOURCES_DIR + "/" + "pseudoBlobRepoDir"));
+        fileSystem.createDirectory(rootDir);
+    }
+
+    @AfterAll
+    public static void removeDirectory() {
+        recursivelyRemoveDirectory(rootDir);
     }
 
     @Test
     public void createPseudoBlobFile() {
-        init.execute();
-        String[] subfoldersAndFiles = new File(rootDir + "/" + OBJECTS_FOLDER_SUB_DIR).list();
-        for (String st: subfoldersAndFiles) {
-            System.out.println("subf" + st);
-        }
-        assertThat(subfoldersAndFiles.length == 0)
-                .withFailMessage("Expected amount of subfolders is 0, but actual is " + subfoldersAndFiles.length)
+        Path objectFolder = Path.of(rootDir + "/" + OBJECTS_FOLDER_SUB_DIR);
+        Path initialFilePath = Path.of(rootDir + "/ oneFile");
+        Path resultingBlobPath = Path.of(objectFolder + "/" + "blobFile");
+        fileSystem.createFile(initialFilePath);
+        fileSystem.putContentToFile(initialFilePath, "Content is sooo content");
+        PseudoBlob.generatePseudoBlobFile(fileSystem, resultingBlobPath, new File(String.valueOf(initialFilePath)));
+        String resultingString = fileSystem.getFileContentsAsString(resultingBlobPath);
+        String[] resultingStringTokenized = resultingString.split(" ");
+        assertThat(resultingStringTokenized[0].equals("blob"))
+                .withFailMessage("Incorrectly generated blob: first token of blob file != 'blob'")
+                .isTrue();
+        assertThat(resultingStringTokenized[1].equals("23"))
+                .withFailMessage("Incorrectly generated blob: second token of blob file != '23'")
+                .isTrue();
+        assertThat(resultingStringTokenized[2].equals("\\0Content"))
+                .withFailMessage("Incorrectly generated blob: third token of blob file != '\\0Content'")
+                .isTrue();
+        String contentLeftover = resultingStringTokenized[3] + " " + resultingStringTokenized[4] + " " + resultingStringTokenized[5];
+        assertThat(contentLeftover.equals("is sooo content"))
+                .as("Incorrectly generated blob: file content is incorrect: expected '" + contentLeftover + "' but was + 'is too content'")
                 .isTrue();
 
+    }
 
-
+    //TODO: to be implemented as well
+    @Test
+    public void createPseudoBlobFromEmptyFile() {
 
     }
+
+
 }
