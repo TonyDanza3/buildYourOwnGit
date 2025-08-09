c0a4bcb829511d587932e4d6647de493f6d7e40a
tonyDanza3 <millermisha5@gmail.com>
2025-08-09 16:50:22 +0200

    implemented Add command class

--- src/main/java/command/commands/add/Add.java
+++ src/main/java/command/commands/add/Add.java
@@ -2,18 +2,24 @@ package command.commands.add;
 
 import command.Command;
 import command.commands.Commands;
+import internal.index.IndexOperations;
+
 import java.nio.file.Path;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class Add extends Command {
 
+    IndexOperations indexOPerations;
+
     public Add() {
         super(Commands.ADD);
+        indexOPerations = new IndexOperations(fileSystem);
     }
 
     public Add(Supplier<Path> getCurrentDirectory, Consumer<String> routeCommandOutput) {
         super(Commands.ADD, getCurrentDirectory, routeCommandOutput);
+        indexOPerations = new IndexOperations(fileSystem);
     }
 
     @Override
@@ -30,7 +36,7 @@ public class Add extends Command {
     @Override
     public void executeCommand(String[]args) {
         for(String arg: args) {
-            //TODO: I stopped here, continue from here
+            indexOPerations.addToIndex(arg);
         }
     }
 }
