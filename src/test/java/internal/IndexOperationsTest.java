db6bf088db73aaa934673a5ea38c9755687dd52f
tonyDanza3 <millermisha5@gmail.com>
2025-11-04 18:13:19 +0100

    implemented first indexOperationsTest

--- src/test/java/internal/IndexOperationsTest.java
+++ src/test/java/internal/IndexOperationsTest.java
@@ -19,8 +19,6 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class IndexOperationsTest {
     private static final Path rootDir = Path.of(RESOURCES_DIR + "/" + "indexOperationsDir");
-    private final Path gitSubdir = Path.of(rootDir + "/" + GIT_FOLDER_NAME);
-    private static final Path objectsDir = Path.of(rootDir + "/" + OBJECTS_FOLDER_SUB_DIR);
     private final FileSystem fileSystem = new FileSystem(() -> rootDir);
 
 
@@ -38,25 +36,24 @@ public class IndexOperationsTest {
     public void addEmptyFileToIndex() {
         Init init = new Init(() -> rootDir, System.out::println);
         Add add = new Add(() -> rootDir, System.out::println);
-        Path newFile = Path.of(rootDir + "/" + "fileInIndex");
-        createFile(newFile);
+        Path fileName = Path.of("fileInIndex");
+        createFile(Path.of(rootDir + "/" + fileName));
         init.execute();
-        List<Path> objectsSubfolders = fileSystem.getSubfoldersFromDirectory(objectsDir, Path.of(gitSubdir
-                + "/" + OBJECTS_INFO.getFolderName()), Path.of(gitSubdir + "/" + OBJECTS_PACK.getFolderName()));
-        assertThat(objectsSubfolders.isEmpty())
-                .withFailMessage(objectsDir + " is not empty. It contains the following dirs:\n" + objectsSubfolders)
-                .isTrue();
-        add.execute(String.valueOf(newFile));
-        /*TODO: assert that:
-            1 file is placed in objects folder
-            2 file is recorded in index file
-         */
-
+        Assertion.checkIndexFileEmpty(fileSystem);
+        add.execute(String.valueOf(fileName));
+        Assertion.fileAddedToIndexFile(fileSystem, fileName);
+        Assertion.fileAddedToIndexFolder(fileSystem, fileName);
     }
 
     @Test
     public void addNonEmptyFileToIndex() {
 
     }
+
+    @Test
+    public void addAddedFileToIndex() {}
+
+    @Test
+    public void addSeveralFilesToIndex() {}
 
 }
