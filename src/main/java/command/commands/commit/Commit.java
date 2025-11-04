367322153549c3601693c57734fb0a8bec8c0353
tonyDanza3 <millermisha5@gmail.com>
2025-11-04 18:15:59 +0100

    temporary commit class impl

--- src/main/java/command/commands/commit/Commit.java
+++ src/main/java/command/commands/commit/Commit.java
@@ -11,6 +11,7 @@ public class Commit extends Command {
     @Override
     protected boolean validateArgs(String[]args) {
         //temporary
+        // можно подсмотреть тут верхнеуровнево как это будет работать и что нужно заимплементить для того чтобы эта команда работала https://chatgpt.com/c/6904a5fd-5784-8330-9f2a-f7addf6c449e
         return true;
     }
 
